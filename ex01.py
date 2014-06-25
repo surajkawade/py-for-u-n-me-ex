c8cc68d40d6ad61c207bbda1f07b63407f240f7d
Suraj Kawade <suraj.kawade@gmail.com>
2014-06-25 12:30:47 +0530

    first 2 ex

---
+++ ex01.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+print "Hello World!"	# This is a comment !!
+print "How are you?"	# TODO -  Ask a question 
+
+
+# GREAT!
---
+++ ex02_modules.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+import math
+print "Hello World!"	# This is a comment !!
+print "How are you?"	# TODO -  Ask a question 
+print e
+
+
+# NOT EXECTUTED 
+# ERROR = root@suraj-Inspiron-N5010:/home/suraj/Documents/PyEx# ./ex02_modules.py 
+# Hello World!
+# How are you?
+# Traceback (most recent call last):
+  # File "./ex02_modules.py", line 5, in <module>
+    # print e
+# NameError: name 'e' is not defined
+# root@suraj-Inspiron-N5010:/home/suraj/Documents/PyEx# 
